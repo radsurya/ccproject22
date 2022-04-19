e1cbc4c7a93dae29198745a0a62f39eb8ce2ae8f
Radica Giva <fc44311@alunos.fc.ul.pt>
2022-04-19 21:43:20 +0100

    Updated db sql files to create and insert data

--- database/initdb1.sql
+++ database/initdb1.sql
@@ -4,35 +4,34 @@ USE mubi_data;
 
 DROP TABLE IF EXISTS movie;
 CREATE TABLE movie (
-	movie_id VARCHAR(255) PRIMARY KEY,
-	movie_title VARCHAR(255),
-	movie_release_year FLOAT,
-	movie_url VARCHAR(255),
-	movie_title_language VARCHAR(255),
-	movie_popularity INT,
-	movie_image_url VARCHAR(255),
-	director_id VARCHAR(255),
-	director_name VARCHAR(255),
-	director_url VARCHAR(255),
-	average_rating FLOAT
+	movie_id VARCHAR(255) PRIMARY KEY, 
+	movie_title VARCHAR(255), 
+	movie_release_year FLOAT, 
+	movie_url VARCHAR(255), 
+	movie_title_language VARCHAR(255), 
+	movie_popularity INT, 
+	movie_image_url VARCHAR(255), 
+	director_id TEXT, 
+	director_name TEXT,
+	director_url TEXT,
+    average_rating FLOAT
 );
-
 
 DROP TABLE IF EXISTS rating;
 CREATE TABLE rating (
-	movie_id VARCHAR(255),
-	rating_id VARCHAR(255) PRIMARY KEY,
-	rating_url VARCHAR(255),
-	rating_score FLOAT,
-	rating_timestamp_utc TIMESTAMP,
-	critic TEXT,
-	critic_likes INT,
-	critic_comments INT,
-	user_id VARCHAR(255),
-	user_trialist BOOLEAN,
-	user_subscriber BOOLEAN,
-	user_eligible_for_trial BOOLEAN,
-	user_has_payment_method BOOLEAN,
+    movie_id VARCHAR(255), 
+    rating_id VARCHAR(255) PRIMARY KEY, 
+    rating_url VARCHAR(255), 
+    rating_score FLOAT,
+    rating_timestamp_utc TIMESTAMP, 
+    critic TEXT, 
+    critic_likes INT, 
+    critic_comments INT, 
+    user_id VARCHAR(255), 
+    user_trialist BOOLEAN, 
+    user_subscriber BOOLEAN,
+    user_eligible_for_trial BOOLEAN, 
+    user_has_payment_method BOOLEAN,
 	FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
 );
 
