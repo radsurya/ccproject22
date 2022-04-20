ce45811f50c925a0245e2c8be8d9da10dc5101ad
Radica Giva <fc44311@alunos.fc.ul.pt>
2022-04-20 22:01:15 +0100

    SQL inserts movie and rating data review

--- database/initdb1.sql
+++ database/initdb1.sql
@@ -35,3 +35,6 @@ CREATE TABLE rating (
 	FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
 );
 
+
+INSERT INTO movie (movie_id, movie_title, movie_release_year, movie_url, movie_title_language, movie_popularity, movie_image_url, director_id, director_name, director_url, average_rating) VALUES ("9000000000", "Movie test Rad", 2022.0, "http://mubi.com/films/claires-knee", "en", 674, "https://images.mubicdn.net/images/film/793/cache-8371-1546689614/image-w1280.jpg", "1057", "Éric Rohmer", "http://mubi.com/cast/eric-rohmer", 0); 
+INSERT INTO rating (movie_id, rating_id, rating_url, rating_score, rating_timestamp_utc, critic, critic_likes, critic_comments, user_id, user_trialist, user_subscriber, user_eligible_for_trial, user_has_payment_method) VALUES ("9000000000", "10704606", "http://mubi.com/films/pavee-lackeen-the-traveller-girl/ratings/10704606", 2.0, "2014-08-15 23:42:31", "", 0, 0, "85981819", True, True, False, True); 
